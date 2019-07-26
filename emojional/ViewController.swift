2446bc44c3dfc603937c9a5ea4cbb3e079f96bd1
Apple <apple@Madj.local>
2019-07-26 14:27:00 -0400

    finished emojinal

--- emojional/ViewController.swift
+++ emojional/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let emojis = ["🤯" : "Stressed?", "🤔" : "Contemplating?" ]
+    
+    let customMessages = ["Stressed?" : ["Take A Water Break!", "Read A Good Book", "Relax", "Jot down new ideas"], "Contemplating?" : ["Go on Tumblr to get writing tips", "Create a new draft", "Re-read your writing", "Discuss your ideas with friends!"]]
+        //
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            // Do any additional setup after loading the view
+    }
+    
+    @IBAction func showMessage(sender: UIButton) {
+        let selectedEmotion = sender.titleLabel?.text
+        
+        let number = Int.random(in: 0...3)
+        
+        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[number]
+        
+       
+        let alertController = UIAlertController(title: emojis[selectedEmotion!], message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
+            
+            alertController.addAction(UIAlertAction(title: "Done.", style: UIAlertAction.Style.default, handler: nil))
+   
+             present(alertController, animated: true, completion: nil)
+        }
+
+    
+        
+       
+
+        
+
     }
 
 
-}
 
